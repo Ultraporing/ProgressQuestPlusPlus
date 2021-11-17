45eb734d8625c96ae6a7298556d0683e3e5b99fc
Tobias Lenz <Black_D@gmx.de>
2021-11-17 11:13:24 +0100

    init UI

---
+++ src/ProgressQuestPlusPlus.cpp
@@ -0,0 +1,46 @@
+﻿// ProgressQuestPlusPlus.cpp : Defines the entry point for the application.
+//
+
+#include "ProgressQuestPlusPlus.h"
+#include "config.h"
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/button.hpp>
+
+
+
+using namespace std;
+
+int main()
+{
+	std::cout << "project name: " << PROJECT_NAME << " version: " << PROJECT_VER << std::endl;
+
+        using namespace nana;
+
+        //Define a form.
+        form fm;
+
+        //Define a label and display a text.
+        label lab{ fm, "Hello, <bold blue size=16>Nana C++ Library</>" };
+        lab.format(true);
+
+        //Define a button and answer the click event.
+        button btn{ fm, "Quit" };
+        btn.events().click([&fm] {
+            fm.close();
+            });
+
+        //Layout management
+        fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
+        fm["text"] << lab;
+        fm["button"] << btn;
+        fm.collocate();
+
+        //Show the form
+        fm.show();
+
+        //Start to event loop process, it blocks until the form is closed.
+        exec();
+
+	return 0;
+}
---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_GUARD
+#define INCLUDE_GUARD
+
+#define PROJECT_NAME "ProgressQuestPlusPlus"
+#define PROJECT_VER  "6.2.0"
+#define PROJECT_VER_MAJOR "6"
+#define PROJECT_VER_MINOR "2"
+#define PTOJECT_VER_PATCH "0"
+
+#endif // INCLUDE_GUARD
